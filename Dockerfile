598671bec0f46933558dc1275dd6961300ff7a88
Mixalis Zois <p10zois@ionio.gr>
2024-05-27 19:12:41 +0300

    update Dockerfile with pandas/openpyxl packages main read upload file (csv/xlsx/xls)

--- Dockerfile
+++ Dockerfile
@@ -4,5 +4,7 @@ FROM python:3.12
 ADD main.py .
 
 RUN pip install streamlit
+RUN pip install pandas
+RUN pip install openpyxl
 
 CMD ["python", "./main.py"]
--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 import streamlit as st
+import pandas as pd
 
 st.title("Welcome to Michalis' Page")
 st.title("Upload a File")
 
 uploaded_file = st.file_uploader("Choose a file", type=['csv', 'xlsx', 'xls'])
+
+if uploaded_file is not None:
+    df = pd.read_csv(uploaded_file) if uploaded_file.name.endswith('.csv') else pd.read_excel(uploaded_file)
+
+    # Display DataFrame
+    st.write(df)
